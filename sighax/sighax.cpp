e277136d87622acc1e3c6be483f446235baf38f3
Myria <myriachan@gmail.com>
2017-01-07 22:58:38 -0800

    Rewrote BruteForce function to use a new method of generating possibilities: multiplying by an existing FIRM signature.  This allows an optimization based on the fact that a decrypted legitimate signature block has a lot of 1 bits in a row, allowing a partial shift-based multiply.

--- sighax/sighax.cpp
+++ sighax/sighax.cpp
@@ -7,12 +7,15 @@
 // * plutoo: Hint on doing negation to get more bang for the mpz_mul buck.
 // * Normmatt: Ported to Windows.
 
+//#define PROFILE_MODE
+
 #include <cinttypes>
 #include <climits>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <ctime>
 #include <limits>
 
 #ifdef _WIN32
@@ -20,7 +23,6 @@
 	#define NOGDI
 	#define NOUSER
 	#include <Windows.h>
-
 	// Use MPIR in Windows.
 	#include <mpir.h>
 #else
@@ -41,7 +43,20 @@ using std::uint8_t;
 using std::uint32_t;
 using std::uint64_t;
 
-enum : unsigned { KEY_SIZE = 256 };
+enum : unsigned
+{
+	KEY_BITS = 2048,
+	KEY_SIZE = KEY_BITS / CHAR_BIT,
+	KEY_LIMB_SIZE = KEY_BITS / std::numeric_limits<mp_limb_t>::digits,
+};
+
+static_assert(KEY_BITS % std::numeric_limits<mp_limb_t>::digits == 0, "bad mp_limb_t size");
+
+// Useful types.
+typedef mp_limb_t Single[KEY_LIMB_SIZE];
+typedef mp_limb_t Double[2 * KEY_LIMB_SIZE];
+typedef mp_limb_t Quad[4 * KEY_LIMB_SIZE];
+
 
 // big-endian
 const char s_modulusText[] =
@@ -57,7 +72,7 @@ const char s_modulusText[] =
 const unsigned long s_publicExponent = 65537;
 
 // sample signed message
-const char s_sample[] =
+const char s_signature[] =
 	"C26EAACEDD4FF31CD970262B2A6BE06D5CEC1115528CAA6F00BADA3A6B9A886B"
 	"5E35DE4FB7E9E4356C4B06B310CCA15AED2B7B433DAB681B0366CC3C769F6D35"
 	"79E6B816A8F01BE9C58C1A61A5AB817E2C2FC55C8C70F584D8D485E75584D71A"
@@ -158,228 +173,263 @@ void DumpNumber(mpz_t number)
 }
 
 
+// Exports a mpz_t as an array of mp_limb_t for mpn_* use.
+template <size_t S>
+void ToLimbArray(mp_limb_t(&limbs)[S], mpz_t number)
+{
+	if (mpz_sizeinbase(number, 2) > S * std::numeric_limits<mp_limb_t>::digits)
+	{
+		std::abort();
+	}
+
+	size_t count;
+	mpz_export(limbs, &count, -1, sizeof(mp_limb_t), 0, 0, number);
+	std::memset(&limbs[count], 0, (S - count) * sizeof(mp_limb_t));
+}
+
+
 // Testing flag.
 volatile bool g_meow = false;
 
-bool IsWhatWeWant(mpz_t number)
+// Determine whether the given buffer is what we want.
+bool IsWhatWeWant(const mp_limb_t *limbs)
 {
-	// NOTE: ToArray outputs little-endian!
-	uint8_t buffer[KEY_SIZE];
-	ToArray(buffer, number);
+	// Test code - used when profiling so that we never find anything.
+#ifdef PROFILE_MODE
+	if (!g_meow) return false;
+#endif
 
-//if (!g_meow) { return false; }
+	// For our own sanity, we use big-endian indexing here.
+	// It's much easier to conceptualize that way.
+	auto getByte = [&limbs](std::size_t index) -> const unsigned char &
+	{
+		// unsigned char is allowed to alias in C/C++ rules.
+		static_assert((KEY_SIZE & (KEY_SIZE - 1)) == 0, "KEY_SIZE must be a power of 2");
+		return (reinterpret_cast<const unsigned char *>(limbs))[index ^ (KEY_SIZE - 1)];
+	};
 
-	if ((buffer[0xFF] != 0x00) || (buffer[0xFE] != 0x02))
+	// A match must begin with 00 02.
+	if ((getByte(0x00) != 0x00) || (getByte(0x01) != 0x02))
 	{
 		return false;
 	}
 
-	// Find streak of nonzero values
+	// Count how many nonzero bytes are after the 0x02.
 	unsigned zeroIndex;
-	for (zeroIndex = 0xFD; zeroIndex > 4; --zeroIndex)
+	for (zeroIndex = 0x02; zeroIndex < KEY_SIZE; ++zeroIndex)
 	{
-		if (buffer[zeroIndex] == 0x00)
+		if (getByte(zeroIndex) == 0x00)
 		{
 			break;
 		}
 	}
 
-	if ((zeroIndex <= 4) || (zeroIndex >= 0xF5))
+	if (zeroIndex >= KEY_SIZE)
 	{
 		return false;
 	}
 
-	if (buffer[zeroIndex - 1] != 0x30)
-	{
-		return false;
-	}
-
-	if (buffer[zeroIndex - 3] != 0x30)
-	{
-		return false;
-	}
+	// TODO: Rest of implementation.
 
 	return true;
 }
 
 
-void BruteForce(mpz_t modulus)
+void BruteForce(mpz_t mpzModulus, mpz_t mpzSignature, mpz_t mpzBlock, unsigned long long numIterations)
 {
-	typedef decltype(0u + uint_fast64_t()) CounterType;
-	enum : unsigned { NUM_FACTORS = 64 };
-	enum : unsigned { ALLOC_BITS = (KEY_SIZE * CHAR_BIT) + (sizeof(mp_limb_t) * CHAR_BIT) };
-//	enum : unsigned long long { NUM_ITERATIONS = 100000 };
-//	enum : unsigned long long { NUM_ITERATIONS = 10000000 };
-	enum : unsigned long long { NUM_ITERATIONS = static_cast<unsigned long long>(-1) >> 1 };
-
-	static_assert(std::numeric_limits<CounterType>::digits >= NUM_FACTORS, "NUM_FACTORS is too small");
-
-	std::printf("Randomly choosing base factors...\n");
-	std::fflush(stdout);
-
-	mpz_t base[NUM_FACTORS];
-	mpz_t forward[NUM_FACTORS];
-	mpz_t backward[NUM_FACTORS];
-
-	for (unsigned x = 0; x < NUM_FACTORS; ++x)
+	enum
 	{
-		for (;;)
+		ATTEMPTS_PER_RANDOM = 1048576,
+		PADDING_SHIFT_BITS = KEY_BITS - 15,
+		LIMB_BITS = std::numeric_limits<mp_limb_t>::digits,
+	};
+
+	mpz_t mpzRandomBase;
+	mpz_init(mpzRandomBase);
+	mpz_t mpzRandomPowered;
+	mpz_init(mpzRandomPowered);
+
+	mpz_t mpzFoundBase;
+	mpz_init(mpzFoundBase);
+	mpz_t mpzFoundPowered;
+	mpz_init(mpzFoundPowered);
+
+	// Convert the modulus and signature to mpn form.
+	Single modulus;
+	ToLimbArray(modulus, mpzModulus);
+
+	Single signature;
+	ToLimbArray(signature, mpzSignature);
+
+	Single block;
+	ToLimbArray(block, mpzBlock);
+
+	// This is the PKCS #1 "01 FF FF FF ..." padding plus one.
+	Single paddingBase;
+	std::memset(paddingBase, 0, sizeof(paddingBase));
+	paddingBase[PADDING_SHIFT_BITS / LIMB_BITS] = (0u + mp_limb_t(1)) << (PADDING_SHIFT_BITS % LIMB_BITS);
+
+	// paddingBase minus the block, giving the multiplier to subtract.
+	Single paddingNegLong;
+	mpn_sub_n(paddingNegLong, paddingBase, block, countof(block));
+
+	mp_limb_t paddingNegShort[7];
+	std::memcpy(paddingNegShort, paddingNegLong, sizeof(paddingNegShort));
+
+	// Main loop.
+	Single randomBase;
+	Double current[2];
+
+	for (unsigned long long iteration = 0; iteration < numIterations; ++iteration)
+	{
+		unsigned index = static_cast<unsigned>(iteration % 2);
+
+		Double &prev = current[index ^ 1];
+		Double &next = current[index];
+
+		// Every so often, we need to reset the random number, otherwise it'll be hard to calculate
+		// the signature later.  By resetting every ATTEMPTS_PER_RANDOM times, we limit the time
+		// spent calculating the signature after finding an answer to ATTEMPTS_PER_RANDOM-1 multiplies.
+		if (iteration % ATTEMPTS_PER_RANDOM == 0)
 		{
-			uint8_t random[KEY_SIZE];
-			ReadRandom(random, sizeof(random));
+			// Choose a random number.  Technically, we want 1 < r < n-1, but the three
+			// degenerate numbers are so incredibly unlikely that we don't need to check for them.
+			// (If they do hit, all that'll happen is that we'll never find a match this round.)
+			ReadRandom(randomBase, sizeof(randomBase));
 
-			mpz_init2(base[x], ALLOC_BITS);
-			mpz_import(base[x], KEY_SIZE, 1, 1, 1, 0, random);
-			mpz_mod(base[x], base[x], modulus);
+			mpz_import(mpzRandomBase, countof(randomBase), -1, sizeof(*randomBase), 0, 0, randomBase);
+			mpz_mod(mpzRandomBase, mpzRandomBase, mpzModulus);
+			ToLimbArray(randomBase, mpzRandomBase);
 
-			if (mpz_cmp_si(base[x], 1) <= 0)
+			// Calculate the 65537th power of this number.
+			mpz_powm_ui(mpzRandomPowered, mpzRandomBase, s_publicExponent, mpzModulus);
+			ToLimbArray(current[index], mpzRandomPowered);
+		}
+		else
+		{
+			// Multiply by the decrypted signature block.  We take advantage of a simple math
+			// fact combined with the PKCS #1 signature format:
+			//
+			// 0x1FFFFFFFFFFF..00<408 other bits> * x
+			//    =
+			// (0x200000000000..00 * x) - ((0x200000000000..00 - 0x1FFFFFFFFFFF..00<408 other bits>) * x)
+			//
+			// This makes the multiplication a 2048x408 multiplication, a 2033-bit left shift,
+			// and a 4096-bit subtract.  That is faster than either a 2048x2048 multiply or a
+			// 2048-bit squaring, which are the alternatives.
+
+			// Multiply by the low 408 bits of the signature block.
+			mpn_mul(next, prev, KEY_LIMB_SIZE, paddingNegShort, countof(paddingNegShort));
+
+			// Multiplying by paddingBase is a left shift.
+			prev[(PADDING_SHIFT_BITS / LIMB_BITS) + KEY_LIMB_SIZE] = mpn_lshift(&prev[PADDING_SHIFT_BITS / LIMB_BITS], prev, KEY_LIMB_SIZE, PADDING_SHIFT_BITS % LIMB_BITS);
+			std::memset(prev, 0, (PADDING_SHIFT_BITS / LIMB_BITS) * sizeof(prev[0]));
+
+			// Subtract off the result of the 2048x408-bit multiply.
+			mpn_sub(next, prev, 2 * KEY_LIMB_SIZE, next, KEY_LIMB_SIZE + countof(paddingNegShort));
+
+			// Reduce the product modulo the key modulus.  This is now the dominating force
+			// in performance, since it's significantly more expensive than the multiplication
+			// trick above.
+			Double dummyQuotient;
+			mpn_tdiv_qr(dummyQuotient, next, 0, next, 2 * KEY_LIMB_SIZE, modulus, KEY_LIMB_SIZE);
+		}
+
+		// Check for matches.
+		// Note that we can check the negative for a second result as well, because
+		// (-x)^65537 == -(x^65537) for any x (because 65537 is odd).
+		// Someone in a math IRC channel pointed out that this is equivalent to multiplication
+		// by another number whose 65537th root we know: namely, -1, whose root is itself.
+		Double *match = nullptr;
+		bool negative = false;
+
+		// TODO: A very slightly faster implementation would be to check whether the highest
+		// limb could possibly be 0x0002____ when negated, avoiding subtraction.  This can
+		// only happen when the highest limb is 0xDECD____ or 0xDECC____.
+		if (IsWhatWeWant(next))
+		{
+			match = &next;
+			negative = false;
+		}
+		else
+		{
+			// Calculate the negative mod modulus.
+			mpn_sub_n(prev, modulus, next, KEY_LIMB_SIZE);
+			if (IsWhatWeWant(prev))
 			{
-			my_continue:
-				continue;
+				match = &prev;
+				negative = true;
+			}
+		}
+
+		// Is this what we want?
+		if (match)
+		{
+			// Calculate randomBase*(signature^iteration).
+			mpz_set(mpzFoundBase, mpzRandomBase);
+
+			unsigned long long squareCount = iteration % ATTEMPTS_PER_RANDOM;
+			for (unsigned long long i = 0; i < squareCount; ++i)
+			{
+				mpz_mul(mpzFoundBase, mpzFoundBase, mpzSignature);
+				mpz_mod(mpzFoundBase, mpzFoundBase, mpzModulus);
 			}
 
-			for (unsigned y = 0; y < x; ++y)
+			// If it's the negative case, negate the base.
+			if (negative)
 			{
-				if (mpz_cmp(base[y], base[x]) == 0)
-				{
-					goto my_continue;
-				}
+				mpz_sub(mpzFoundBase, mpzModulus, mpzFoundBase);
 			}
 
-			break;
-		}
+			// Calculate foundBase^65537.
+			mpz_powm_ui(mpzFoundPowered, mpzFoundBase, s_publicExponent, mpzModulus);
 
-		mpz_init2(forward[x], ALLOC_BITS);
-		mpz_powm_ui(forward[x], base[x], s_publicExponent, modulus);
+			// Export these numbers.
+			Single foundBase;
+			ToLimbArray(foundBase, mpzFoundBase);
+			Single foundPowered;
+			ToLimbArray(foundPowered, mpzFoundPowered);
 
-		mpz_init2(backward[x], ALLOC_BITS);
-		if (mpz_invert(backward[x], forward[x], modulus) == 0)
-		{
-			// You're more likely to win the lottery several times in a row
-			// than for this to happen.
-			std::printf("Public key factored by accident!!!  Give this to Myria!\n");
-			DumpNumber(forward[x]);
-			std::fflush(stdout);
-			std::exit(1);
-		}
-
-		// Self-test
-		mpz_t test;
-		mpz_init2(test, ALLOC_BITS * 2);
-		mpz_mul(test, forward[x], backward[x]);
-		mpz_mod(test, test, modulus);
-		if (mpz_cmp_si(test, 1) != 0)
-		{
-			std::printf("Self-test error\n");
-			DumpNumber(base[x]);
-			std::fflush(stdout);
-			std::exit(1);
-		}
-		mpz_clear(test);
-	}
-
-	std::printf("Brute-forcing...\n");
-	std::fflush(stdout);
-
-	CounterType counter = 0;
-
-	mpz_t current;
-	mpz_init2(current, ALLOC_BITS * 2);
-	mpz_set_ui(current, 1);
-
-	mpz_t negative;
-	mpz_init2(negative, ALLOC_BITS);
-
-	mpz_t *possibilities[] = { &current, &negative };
-	bool isNegative[] = { false, true };
-
-	for (unsigned long long iteration = 0; iteration < NUM_ITERATIONS; ++iteration)
-	{
-		CounterType grayPrevious = counter ^ (counter >> 1);
-		++counter;
-		CounterType grayCurrent = counter ^ (counter >> 1);
-
-		CounterType flip = grayCurrent ^ grayPrevious;
-
-		if ((flip == 0) || ((flip & (flip - 1)) != 0))
-		{
-			std::printf("Gray counter error\n");
-			std::fflush(stdout);
-			std::exit(1);
-		}
-
-		int flipIndex = std::numeric_limits<CounterType>::digits - 1 - CountLeadingZeros(flip);
-
-		// Determine whether the bit is being turned on or off.
-		bool on = (flip & grayCurrent) != 0;
-
-//		std::printf("%3llu: %08X %08X %08X %d %d\n", iteration, (unsigned) counter, (unsigned) grayCurrent, (unsigned) flip, flipIndex, on);
-
-		// Calculate next iteration.
-		mpz_mul(current, current, (on ? forward : backward)[flipIndex]);
-		mpz_mod(current, current, modulus);
-
-		mpz_sub(negative, modulus, current);
-
-		// Is this iteration what we're looking for?
-		for (unsigned index = 0; index < countof(possibilities); ++index)
-		{
-			mpz_t &checking = *possibilities[index];
-			if (IsWhatWeWant(checking))
+			// Verify that foundPowered matches the number we found.
+			if (std::memcmp(foundPowered, *match, sizeof(Single)) != 0)
 			{
-				mpz_t testPower, testBase, temp;
-				mpz_init_set_ui(testPower, 1);
-				mpz_init_set_ui(testBase, 1);
-				mpz_init2(temp, ALLOC_BITS);
-				for (int x = 0; x < std::numeric_limits<CounterType>::digits; ++x)
-				{
-					CounterType bit = CounterType(1) << x;
-					if (grayCurrent & bit)
-					{
-						mpz_mul(testPower, testPower, forward[x]);
-						mpz_mod(testPower, testPower, modulus);
-						mpz_mul(testBase, testBase, base[x]);
-						mpz_mod(testBase, testBase, modulus);
-					}
-				}
-				if (isNegative[index])
-				{
-					mpz_sub(testPower, modulus, testPower);
-					mpz_sub(testBase, modulus, testBase);
-				}
-				if (mpz_cmp(testPower, checking) != 0)
-				{
-					std::printf("Multiplication build error\n");
-					std::fflush(stdout);
-					std::exit(1);
-				}
-				mpz_powm_ui(temp, testBase, s_publicExponent, modulus);
-				if (mpz_cmp(temp, testPower) != 0)
-				{
-					std::printf("Exponential build error\n");
-					std::fflush(stdout);
-					std::exit(1);
-				}
-
-				std::printf("Match found!!  (mode=%s)\n", isNegative[index] ? "positive" : "negative");
-				std::printf("iteration = %llu\n", iteration);
-				std::printf("Buffer:\n");
-				DumpNumber(temp);
-				std::printf("Signature:\n");
-				DumpNumber(testBase);
+				std::printf("Exponential build error\n");
 				std::fflush(stdout);
-
-				mpz_clears(testPower, testBase, temp, nullptr);
-				//break;
+				std::exit(1);
 			}
+
+			std::printf("Match found!!  (mode=%s)\n", (!negative) ? "positive" : "negative");
+			std::printf("iteration = %llu\n", iteration);
+			std::printf("Buffer:\n");
+			DumpNumber(mpzFoundPowered);
+			std::printf("Signature:\n");
+			DumpNumber(mpzFoundBase);
+			std::fflush(stdout);
 		}
+
+		iteration = iteration;
 	}
 
-	mpz_clears(current, negative, nullptr);
-	for (unsigned x = 0; x < NUM_FACTORS; ++x)
-	{
-		mpz_clears(base[x], forward[x], backward[x], nullptr);
-	}
+	mpz_clears(mpzRandomBase, mpzRandomPowered, mpzFoundBase, mpzFoundPowered, nullptr);
+}
+
+
+void Profile(void(*function)(mpz_t, mpz_t, mpz_t, unsigned long long), mpz_t modulus, mpz_t signature, mpz_t block)
+{
+	enum : unsigned long long { NUM_ITERATIONS = 10000000 };
+
+	std::clock_t start = std::clock();
+
+	function(modulus, signature, block, NUM_ITERATIONS);
+
+	std::clock_t end = std::clock();
+
+	// Multiplying NUM_ITERATIONS by 2 because the brute forcer is checking
+	// positive and negative numbers each iteration.
+	std::printf("%llu operations in %g seconds (%llu/second)\n",
+		static_cast<unsigned long long>(NUM_ITERATIONS * 2),
+		static_cast<double>(end - start) / CLOCKS_PER_SEC,
+		static_cast<unsigned long long>((NUM_ITERATIONS * 2) / (static_cast<double>(end - start) / CLOCKS_PER_SEC)));
 }
 
 
@@ -402,15 +452,22 @@ int main()
 	mpz_t modulus;
 	mpz_init_set_str(modulus, s_modulusText, 16);
 
-	mpz_t sample;
-	mpz_init_set_str(sample, s_sample, 16);
+	mpz_t signature;
+	mpz_init_set_str(signature, s_signature, 16);
 
-	mpz_powm_ui(sample, sample, s_publicExponent, modulus);
+	mpz_t block;
+	mpz_init(block);
+
+	mpz_powm_ui(block, signature, s_publicExponent, modulus);
 
 	//DumpNumber(sample);
 
-	BruteForce(modulus);
+#ifdef PROFILE_MODE
+	Profile(BruteForce, modulus, signature, block);
+#else
+	BruteForce(modulus, signature, block, 10000000);
+#endif
 
-	mpz_clears(modulus, sample, nullptr);
+	mpz_clears(modulus, signature, block, nullptr);
 	return 0;
 }
