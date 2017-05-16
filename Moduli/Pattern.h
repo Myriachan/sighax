fb19bfbeeb51e1ff2160a3f15bfe8024e702dce8
Myria <myriachan@gmail.com>
2017-05-15 22:05:13 -0700

    Final version of Pattern.h that finds real signatures.

--- Moduli/Pattern.h
+++ Moduli/Pattern.h
@@ -24,9 +24,9 @@ __device__ __host__ bool IsWhatWeWant(const GetByteLambda &getByte)
 		return false;
 	}
 
-	// Count how many nonzero bytes are after the 0x02.
+	// Find a 00 byte.
 	unsigned zeroIndex;
-	for (zeroIndex = 0x02; zeroIndex < KEY_SIZE; ++zeroIndex)
+	for (zeroIndex = 2; zeroIndex < KEY_SIZE; ++zeroIndex)
 	{
 		if (getByte(zeroIndex) == 0x00)
 		{
@@ -34,12 +34,91 @@ __device__ __host__ bool IsWhatWeWant(const GetByteLambda &getByte)
 		}
 	}
 
-	if (zeroIndex >= KEY_SIZE)
+	if (zeroIndex >= KEY_SIZE - 1 - 1)
 	{
 		return false;
 	}
 
-	// TODO: Rest of implementation.
+	// Rest of signature must be no bigger than 0x33 bytes.
+	if (zeroIndex < KEY_SIZE - 0x33 - 1)
+	{
+		return false;
+	}
 
-	return true;
+	// Check for the first DER number having a 0x30 type.
+	if (getByte(zeroIndex + 1) != 0x30)
+	{
+		return false;
+	}
+
+	// Verify the size.
+	unsigned firstNumSize = getByte(zeroIndex + 2);
+	if (firstNumSize < 0x80)
+	{
+		firstNumSize = 1;
+	}
+	else if (firstNumSize > 0x84)
+	{
+		return false;
+	}
+	else
+	{
+		firstNumSize = 1 + (firstNumSize & 0x7F);
+	}
+
+	// Verify that that many bytes remain in the buffer.
+	if (zeroIndex + 2 + firstNumSize + 1 + 1 > KEY_SIZE)
+	{
+		return false;
+	}
+
+	// Index of start of next number.
+	unsigned secondNumIndex = zeroIndex + 2 + firstNumSize;
+
+	// Verify that the second number sequence starts with 30.
+	if (getByte(secondNumIndex) != 0x30)
+	{
+		return false;
+	}
+
+	// Read the second number.
+	unsigned finalIndex = secondNumIndex + 1;
+	if (finalIndex >= KEY_SIZE)
+	{
+		return false;
+	}
+
+	unsigned secondNumValue;
+	unsigned secondNumHeader = getByte(finalIndex);
+	++finalIndex;
+
+	if (secondNumHeader < 0x80)
+	{
+		secondNumValue = secondNumHeader;
+	}
+	else if (secondNumHeader < 0x85)
+	{
+		unsigned size = (secondNumHeader & 0x7F);
+		secondNumValue = 0;
+		for (unsigned x = 0; x < size; ++x)
+		{
+			if (finalIndex >= KEY_SIZE)
+			{
+				return false;
+			}
+
+			secondNumValue <<= 8;
+			secondNumValue += getByte(finalIndex);
+			++finalIndex;
+		}
+	}
+	else
+	{
+		// Boot9 will reject this.
+		return false;
+	}
+
+	// Finally, check the offset as being 2 from the end.
+	unsigned final = finalIndex + secondNumValue;
+	return final == KEY_SIZE - 2;
 }
